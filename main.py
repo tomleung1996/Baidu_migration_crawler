6fbcfa3270b1260ead6a69c55163c5dc0989202f
tomleung1996 <tomleung1996@qq.com>
2020-02-01 16:28:16 +0800

    Checkpoint 2: Fetch all cities in a given time range, and the national distribution

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import requests
 import json
 import pymongo
 from tqdm import tqdm
+import datetime
 
 
-def fetch_single_data(level, move_in, location_id, date):
+def fetch_one_loc(level, move_in, location_id, date):
     """
     爬取百度迁徙的人口流入/流出分布情况和绝对数字（数字含义未知，应能表示相对大小）
 
@@ -33,6 +34,8 @@ def fetch_single_data(level, move_in, location_id, date):
 
     if str(location_id).endswith('0000'):
         dtype = 'province'
+    elif location_id == 0:
+        dtype = 'country'
     else:
         dtype = 'city'
 
@@ -46,6 +49,24 @@ def fetch_single_data(level, move_in, location_id, date):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
     }
+
+    if dtype == 'country':
+        dis_result = requests.get(dis_url, headers=headers).text
+        start_1 = dis_result.find('(')
+        end_1 = dis_result.rfind(')')
+        dis_result = json.loads(dis_result[start_1 + 1:end_1])['data']['list']
+
+        final_result = {
+            'location': 'China',
+            'code': 0,
+            'type': 'country',
+            'level': level,
+            'move': 'in' if move_in else 'out',
+            'date': date,
+            'distribution': dis_result
+        }
+
+        return final_result
 
     dis_result = requests.get(dis_url, headers=headers).text
 
@@ -65,16 +86,20 @@ def fetch_single_data(level, move_in, location_id, date):
             'distribution': dis_result
         }
     except KeyError:
-        print('KeyError, 提交请求格式可能有问题')
+        print('KeyError, 提交请求格式可能有问题', location_id)
         return None
     except TypeError:
-        print('TypeError, 可能遇到县级市')
+        print('TypeError, 可能遇到县级市', location_id)
         return None
+    except json.JSONDecodeError:
+        print(number_result)
+        print(dis_result)
+        exit(-1)
 
     return final_result
 
 
-def fetch_all_data(loc_file_path:str):
+def fetch_all_loc(loc_file_path: str, date: str):
     loc_id_dict = {}
     res = []
 
@@ -84,10 +109,10 @@ def fetch_all_data(loc_file_path:str):
             loc_id_dict[int(code)] = loc
 
     for code, loc in tqdm(loc_id_dict.items()):
-        print(code, loc)
+        # print(code, loc)
         for level in ['city', 'province']:
             for move_in in [True, False]:
-                single_res = fetch_single_data(level, move_in, code, '20200131')
+                single_res = fetch_one_loc(level, move_in, code, date)
                 if single_res is None:
                     break
 
@@ -97,7 +122,7 @@ def fetch_all_data(loc_file_path:str):
                     'type': 'province' if str(code).endswith('0000') else 'city',
                     'level': level,
                     'move': 'in' if move_in else 'out',
-                    'date': '20200131',
+                    'date': date,
                     'number': single_res['number'],
                     'distribution': single_res['distribution']
                 })
@@ -108,11 +133,29 @@ def fetch_all_data(loc_file_path:str):
     return res
 
 
-if __name__ == '__main__':
-    # fetch_single_data('city', True, '110000', '20200130')
-    result = fetch_all_data(r'location_ids.txt')
+def fetch_timerange(start, end):
+    start = datetime.datetime.strptime(start, '%Y%m%d')
+    end = datetime.datetime.strptime(end, '%Y%m%d')
 
-    # connect_str = "mongodb://tomleung1996:1996821abc@baiduqianxi-shard-00-00-n9mzn.azure.mongodb.net:27017,baiduqianxi-shard-00-01-n9mzn.azure.mongodb.net:27017,baiduqianxi-shard-00-02-n9mzn.azure.mongodb.net:27017/test?ssl=true&replicaSet=BaiduQianxi-shard-0&authSource=admin&retryWrites=true&w=majority"
-    # client = pymongo.MongoClient(connect_str)
-    # client.qianxi.flow.insert_many(result)
-    # client.close()
+    while start <= end:
+        date = start.strftime('%Y%m%d')
+        print(date)
+        result = fetch_all_loc('location_ids.txt', date)
+
+        cn_res_c_i = fetch_one_loc('city', True, 0, date)
+        cn_res_c_o = fetch_one_loc('city', False, 0, date)
+        cn_res_p_i = fetch_one_loc('province', True, 0, date)
+        cn_res_p_o = fetch_one_loc('province', False, 0, date)
+        cn_res = [cn_res_c_i, cn_res_c_o, cn_res_p_i, cn_res_p_o]
+
+        start += datetime.timedelta(days=1)
+
+        connect_str = "mongodb://tomleung1996:1996821abc@baiduqianxi-shard-00-00-n9mzn.azure.mongodb.net:27017,baiduqianxi-shard-00-01-n9mzn.azure.mongodb.net:27017,baiduqianxi-shard-00-02-n9mzn.azure.mongodb.net:27017/test?ssl=true&replicaSet=BaiduQianxi-shard-0&authSource=admin&retryWrites=true&w=majority"
+        client = pymongo.MongoClient(connect_str)
+        client.qianxi.flow.insert_many(result)
+        client.qianxi.cn_distribution.insert_many(cn_res)
+        client.close()
+
+
+if __name__ == '__main__':
+    fetch_timerange('20200101', '20200102')
